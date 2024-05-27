1a7ea632b5be09f32a3cfacfcb2ca609a7e07cc7
OhMinsSup <mins5190@gmail.com>
2024-05-27 17:50:42 +0900

    add: 수정

--- apps/website/eslint.config.js
+++ apps/website/eslint.config.js
@@ -1,4 +1,4 @@
-import baseConfig, { restrictEnvAccess } from "@acme/eslint-config/base";
+import baseConfig, { restrictEnvAccess } from "@thread/eslint-config/base";
 import nextjsConfig from "@thread/eslint-config/nextjs";
 import reactConfig from "@thread/eslint-config/react";
 
